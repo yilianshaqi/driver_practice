119bce361986809c4f05c894326757192100cfd6
yilianshaqi <huangyangwork@sina.com>
2016-12-01 16:23:07 +0800

    unix套接字tcp实现进程通信

---
+++ socket/sock_unix.c
@@ -0,0 +1,71 @@
+/*************************************************************************
+    > File Name: sock_unix.c
+    > Author: huang yang
+    > Mail: huangyangwork@sina.com 
+    > Created Time: 2016年12月01日 星期四 15时17分45秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include<strings.h>
+#include <arpa/inet.h>
+#include<string.h>
+#include <unistd.h>
+ #include <sys/un.h>
+#define COUNT 5
+#define BUF_SIZE 30
+const char socket_name[]="/home/hy/practice/unix";
+int main()
+{
+	struct sockaddr_un saddr;
+	bzero(&saddr,sizeof(saddr));
+	saddr.sun_family = PF_LOCAL;
+	strcpy(saddr.sun_path,socket_name);
+	//创建UNIX socket
+	int fd = socket(PF_LOCAL,SOCK_STREAM,0);
+	if(fd<0)
+	{
+		perror("create socket error\n");
+		return -1;
+	}
+	//绑定socket
+	if(bind(fd,(struct sockaddr *)&saddr,sizeof(saddr)))
+	{
+		perror("bind error\n");
+		return -1;
+	}
+	//listen
+	if(listen(fd,COUNT))
+	{
+		perror("listen error\n");
+		return -1;
+	}
+	//accept
+	int rfd=accept(fd,NULL,NULL);
+	if(rfd<0)
+	{
+		perror("accept error\n");
+		return -1;
+	}
+	//recv
+	char buf[BUF_SIZE]="";
+	int ret =0;
+	int i = 10;
+	while(i--){
+
+
+		ret = recv(rfd,buf,BUF_SIZE,0);
+		if(ret <=0)
+		{
+			perror("recv error:\n");
+			return -1;
+		}
+		printf("ret =%d,recv : %s\n",ret,buf);
+	}
+	//close
+	close(rfd);
+	close(fd);
+	return 0;
+}
+
---
+++ socket/unix_cli.c
@@ -0,0 +1,53 @@
+/*************************************************************************
+    > File Name: client.c
+    > Author: huang yang
+    > Mail: huangyangwork@sina.com 
+    > Created Time: 2016年11月28日 星期一 19时49分18秒
+ ************************************************************************/
+#include<stdio.h>
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include<strings.h>
+#include <arpa/inet.h>
+#include<string.h>
+#include <unistd.h>
+#include <sys/un.h>
+#define BUF_SIZE  30
+const char name[]="/home/hy/practice/unix";
+int main(){
+	int fs=0;
+	int ret =0;
+	char buf[BUF_SIZE]="";
+	struct sockaddr_un my_addr;
+	bzero(&my_addr,sizeof(my_addr));
+	my_addr.sun_family = PF_LOCAL;
+	strcpy(my_addr.sun_path,name);
+	fs = socket(PF_LOCAL,SOCK_STREAM,0);
+	if(fs<=0)
+	{
+		perror("socket :\n");
+		return -1;
+	}
+	ret = connect(fs,(struct sockaddr *)&my_addr,sizeof(my_addr));
+	if(ret<0)
+	{
+		perror("connect :\n");
+		return -1;
+	}
+	int i=10;
+	while(i){
+		sleep(1);
+		strcpy(buf,"test ok !");
+		ret = send(fs,buf,strlen(buf),MSG_DONTWAIT);
+		if(ret<0)
+		{
+			perror("recv : \n");
+			break;
+		}
+		bzero(buf,BUF_SIZE);
+	}
+	close(fs);
+	return 0;
+
+
+}
